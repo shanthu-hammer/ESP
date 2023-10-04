148bb7cb1bd1c27d42ad41ab3a9da91111041401
santhu-hammer <ssanthujan@yahoo.com>
2023-10-04 13:45:50 +0530

    Login page

--- ESP/frontend/src/components/home/Home.js
+++ ESP/frontend/src/components/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../home/Home.css";
 import "../../assets/background.jpg";
 import Login from "../authentication/login/Login";
+import "../../fonts/PublicPixel-z84yD.ttf";
 function Home() {
   return (
     <div className="home">
