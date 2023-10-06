f2040c96352787a68be795941c929efced8c5b4c
santhu-hammer <ssanthujan@yahoo.com>
2023-10-07 04:45:16 +0530

    Login auth

--- ESP/frontend/src/components/authentication/login/Login.js
+++ ESP/frontend/src/components/authentication/login/Login.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../login/Login.css";
 import "../../../assets/background.jpg";
 import { useNavigate } from "react-router-dom";
 import CustomButton from "../../button/CustomButton";
 function Login() {
   const navigate = useNavigate();
+  const loginCredential_default = { id: "default", password: "default" };
+  const [loginCredential, setLoginCredential] = useState(
+    loginCredential_default
+  );
+  const [status, setStatus] = useState(true);
   const handleClick = () => {
-    navigate("/dashboard");
+    // console.log("ID is " + loginCredential.id);
+    // console.log("Passcode is " + loginCredential.password);
+    if (loginCredential.id == "wei" && loginCredential.password == "wei123") {
+      navigate("/dashboard");
+    } else {
+      setStatus(false);
+    }
+  };
+  const HandleOnChange = (e) => {
+    setLoginCredential({
+      ...loginCredential,
+      [e.target.name]: [e.target.value.trim()],
+    });
   };
 
+  useEffect(() => {});
   return (
     <div className="login">
       <div className="login_form">
+        {!status && <h6>Wrong ID or Passcode </h6>}
+
         <h4>ID </h4>
-        <input></input>
-        <h4>PASSCODE</h4>
-        <input type="password"></input>
+        <input
+          onChange={(e) => {
+            HandleOnChange(e);
+          }}
+          name="id"
+        ></input>
+        <h4>PASSWORD</h4>
+        <input
+          onChange={(e) => {
+            HandleOnChange(e);
+          }}
+          type="password"
+          name="password"
+        ></input>
         {/* <button onClick={handleClick}>LOGIN</button> */}
         <CustomButton name="LOGIN" onClick={handleClick} />
       </div>
