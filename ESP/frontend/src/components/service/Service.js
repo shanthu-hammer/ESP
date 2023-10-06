57d7bc4aee169caf5da0c67fa1460c7d70623a1b
santhu-hammer <ssanthujan@yahoo.com>
2023-10-07 03:14:14 +0530

    fetch data btn

--- ESP/frontend/src/components/service/Service.js
+++ ESP/frontend/src/components/service/Service.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "../service/Service.css";
+import CustomButton from "../button/CustomButton";
 function Service({ Logo, name, expiraryDate, amount }) {
+  const HandleClick = (e) => {
+    e.preventDefault();
+    alert("Data has been displayed ");
+  };
+
   return (
     <div className="service">
       <img src={Logo} alt={name}></img>
       <div className="service_description">{name}</div>
       <div className="service_description">{expiraryDate}</div>
       <div className="service_description">¥{amount}</div>
+
+      <div className="service_button"
+        onClick={(e) => {
+          HandleClick(e);
+        }}
+      >
+        Get info{" "}
+      </div>
     </div>
   );
 }
