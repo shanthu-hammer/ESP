6ca3ea199368901bcdc00837b309c3d71d8354ae
santhu-hammer <ssanthujan@yahoo.com>
2023-10-08 21:53:24 +0530

    Fetch file aws

--- ESP/frontend/src/aws_esp/aws_connection.js
+++ ESP/frontend/src/aws_esp/aws_connection.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
 import AWS from "aws-sdk";
-function Aws_connection() {
-  //   const [data, setData] = useState("Defaulttttt");
-
+const Aws_connection = async (fileName) => {
+  console.log("from api key " + process.env.REACT_APP_AWS_ACCESS_KEY);
   AWS.config.update({
-    accessKeyId: "AKIAS7MK75OX4UZWL2JX",
-    secretAccessKey: "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",
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.REACT_APP_SECRECT_ACCESS_KEY,
     //   region: "YOUR_REGION",
   });
   const s3 = new AWS.S3();
   const params = {
     Bucket: "shanthubucket",
-    Key: "test.json",
+    Key: fileName,
+    //header
   };
-
-  s3.getObject(params, (err, data) => {
-    if (err) {
-      console.error("Error:", err);
-    } else {
-      const jsonContent = JSON.parse(data.Body.toString());
-      console.log("JSON Content:", jsonContent);
-      //   setData(jsonContent);
-    }
+  return new Promise((resolve, reject) => {
+    s3.getObject(params, (err, data) => {
+      if (err) {
+        console.error("Error:", err);
+        reject(err); // Reject the Promise with the error
+      } else {
+        const jsonContent = JSON.parse(data.Body.toString());
+        console.log(jsonContent);
+        resolve(jsonContent); // Resolve the Promise with the fetched data
+      }
+    });
   });
-}
+};
 
 export default Aws_connection;
