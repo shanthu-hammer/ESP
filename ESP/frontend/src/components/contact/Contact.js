11f8a2f2de3dd7e51093947b5bfee23958e8b6f2
santhu-hammer <ssanthujan@yahoo.com>
2023-10-10 02:53:40 +0530

    Nav bar fix

---
+++ ESP/frontend/src/components/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../contact/Contact.css";
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="contact_header">
+        <div className="contact_header_name">Contact Page</div>
+        <div className="contact_header_back">Back </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
--- ESP/frontend/src/components/navbar/Navbar.js
+++ ESP/frontend/src/components/navbar/Navbar.js
@@ -2,12 +2,35 @@ import React from "react";
 import CustomButton from "../button/CustomButton";
 import "../navbar/Navbar.css";
 import "../../assets/buttonBackGround.png";
-const Navbar = () => {
+const Navbar = ({ ContactClick, SignoutClick }) => {
+  const ContactHandleClick = () => {
+    ContactClick();
+  };
+  const ContactSignoutClick = () => {
+    SignoutClick();
+  };
   return (
     <div className="navbar">
       <div className="navbar_heading">Entertainment Service Packages </div>
-      <div className="navbar_contact">Contact</div>
-      <div className="navbar_signout">Signout</div>
+      <div className="navbar_buttons">
+        {" "}
+        <div
+          className="navbar_buttons_contact"
+          onClick={() => {
+            ContactHandleClick();
+          }}
+        >
+          Contact
+        </div>
+        <div
+          className="navbar_buttons_signout"
+          onClick={() => {
+            ContactSignoutClick();
+          }}
+        >
+          Signout
+        </div>
+      </div>
     </div>
   );
 };
